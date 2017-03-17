5bea444dac6b0fdc53f3ae3364006d88e8b77433
Linker <285842259@qq.com>
2017-03-17 17:02:28 +0800

    Add files via upload

---
+++ test.js
@@ -0,0 +1,38 @@
+﻿var app1 = new Vue({
+  el: '#app1',
+  data: {
+    message: '<h1>WelCome to Vue!</h1>'
+  }
+});
+
+var app2 = new Vue({
+  el: '#app2',
+  data: {
+    message: 'WelCome to Vue!'
+  }
+});
+
+var app3 = new Vue({
+  el: '#app3',
+  data: {
+    message: 'Now is ' + new Date()
+  }
+});
+
+var app4 = new Vue({
+  el: '#app4',
+  data: {
+    seen: true
+  }
+});
+
+var app5 = new Vue({
+  el: '#app5',
+  data: {
+    todos: [
+      {text: 'study javascript'},
+      {text: 'study Vue'},
+      {text: 'all projects'}
+	]
+  }
+})
